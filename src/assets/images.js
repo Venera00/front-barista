d123db01213a7a6a2816feb8160c2d4941adcc07
Venera <veneraomurzakkyzy@gmail.com>
2024-02-08 15:07:41 +0600

    Fix image import

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,12 +1,21 @@
+import loginImg from "./loginImg.png";
+import neocafeIconLogin from "./neocafeIconLogin.svg";
+import neocafeMainIcon from "./neocafeMainIcon.svg";
+import ordersIcon from "./ordersIcon.svg";
+import profileIcon from "./profileIcon.svg";
+import menuIcon from "./menuIcon.svg";
+import bellIcon from "./bellIcon.svg";
+import logoutIcon from "./logoutIcon.svg";
+
 const images = {
-  loginImg: require("./loginImg.png").default,
-  neocafeIconLogin: require("./neocafeIconLogin.svg").default,
-  neocafeMainLogin: require("./neocafeMainLogin.svg").default,
-  ordersIcon: require("./ordersIcon.svg").default,
-  profileIcon: require("./profileIcon.svg").default,
-  menuIcon: require("./menuIcon.svg").default,
-  bellIcon: require("./bellIcon.svg").default,
-  logoutIcon: require("./logoutIcon.svg").default,
+  loginImg: loginImg,
+  neocafeIconLogin: neocafeIconLogin,
+  neocafeMainIcon: neocafeMainIcon,
+  ordersIcon: ordersIcon,
+  profileIcon: profileIcon,
+  menuIcon: menuIcon,
+  bellIcon: bellIcon,
+  logoutIcon: logoutIcon,
 };
 
 export default images;
