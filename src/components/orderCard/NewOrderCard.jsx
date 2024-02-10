8c2ce8b079197e1d5c7a5de6f3430003c75dd885
Venera <veneraomurzakkyzy@gmail.com>
2024-02-10 14:29:41 +0600

    T4-88 finish the statuses of orders

---
+++ src/components/orderCard/NewOrderCard.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import images from "../../assets/images";
+// import styles from "./OrderCard.module.scss";
+
+// const newOrderCard = ({ order }) => {
+//   return (
+//     <div className={styles.neworder_card}>
+//       <img src={images.closeIcon} alt="Close" />
+//       <h3>{order.number}</h3>
+//       <p>{order.waiter}</p>
+//       <p>{order.description}</p>
+
+//       <button>Принять</button>
+//     </div>
+//   );
+// };
+
+// export default newOrderCard;
--- src/pages/OrdersPage/OrdersPage.jsx
+++ src/pages/OrdersPage/OrdersPage.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import images from "../../assets/images";
+// import NewOrderCard from "../../components/orderCard/NewOrderCard";
 import styles from "./OrdersPage.module.scss";
 
 const OrdersPage = () => {
+  const [selectedOrderType, setSelectedOrderType] = useState("takeaway");
+  const [showNewOrders, setShowNewOrders] = useState(true);
+
+  const handleOrderTypeSelection = (ordertype) => {
+    setSelectedOrderType(ordertype);
+  };
+
+  const handleShowNewOrders = () => {
+    setShowNewOrders(true);
+  };
+
+  const newOrders = [
+    {
+      id: 1,
+      status: "новый",
+      number: "M-47",
+      waiter: "Валентина",
+      description: "New order 1",
+    },
+    {
+      id: 2,
+      status: "новый",
+      number: "M-47",
+      waiter: "Валентина",
+      description: "New order 1",
+    },
+  ];
+
   return (
     <div className={styles.orders_container}>
       <div className={styles.sidebar}>
@@ -39,10 +68,20 @@ const OrdersPage = () => {
       <div className={styles.ordertype_container}>
         <div className={styles.ordertype_wrapper}>
           <h3 className={styles.orders_title}>Заказы</h3>
-          <button className={`${styles.orderplace_btn} ${styles.takeaway_btn}`}>
+          <button
+            className={`${styles.orderplace_btn} ${styles.takeaway_btn} ${
+              selectedOrderType === "takeaway" ? styles.selectedOrderType : ""
+            }`}
+            onClick={() => handleOrderTypeSelection("takeaway")}
+          >
             На вынос
           </button>
-          <button className={`${styles.orderplace_btn} ${styles.inplace_btn}`}>
+          <button
+            className={`${styles.orderplace_btn} ${styles.inplace_btn} ${
+              selectedOrderType === "inplace" ? styles.selectedOrderType : ""
+            }`}
+            onClick={() => handleOrderTypeSelection("inplace")}
+          >
             В заведении
           </button>
           <img
@@ -55,32 +94,59 @@ const OrdersPage = () => {
         <div className={styles.orders}>
           <div className={styles.orderstatus_wrapper}>
             <button
-              className={`${styles.orderstatus_btn} ${styles.orderstatus_new}`}
+              className={`${styles.orderstatus_btn} ${styles.orderstatus_new} ${
+                showNewOrders ? styles.selectedOrderType : ""
+              }`}
+              onClick={handleShowNewOrders}
             >
+              <span
+                className={`${styles.order_status} ${styles.status_new}`}
+              ></span>
               Новые
             </button>
             <button
               className={`${styles.orderstatus_btn} ${styles.orderstatus_inprocess}`}
             >
+              <span
+                className={` ${styles.order_status} ${styles.status_inprocess}`}
+              ></span>
               В процессе
             </button>
-            <button
-              className={`${styles.orderstatus_btn} ${styles.orderstatus_ready}`}
-            >
-              Готово
-            </button>
+            {selectedOrderType === "takeaway" ? null : (
+              <button
+                className={`${styles.orderstatus_btn} ${styles.orderstatus_ready}`}
+              >
+                <span
+                  className={`${styles.order_status} ${styles.status_ready}`}
+                ></span>
+                Готово
+              </button>
+            )}
 
             <button
               className={`${styles.orderstatus_btn} ${styles.orderstatus_cancelled}`}
             >
+              <span
+                className={`${styles.order_status} ${styles.status_cancelled}`}
+              ></span>
               Отменено
             </button>
             <button
               className={`${styles.orderstatus_btn} ${styles.orderstatus_done}`}
             >
+              <span
+                className={`${styles.order_status} ${styles.status_done}`}
+              ></span>
               Завершено
             </button>
           </div>
+
+          {/* <div className={styles.ordercard_wrapper}>
+            {showNewOrders &&
+              newOrders.map((order) => (
+                <NewOrderCard key={order.id} order={order} />
+              ))}
+          </div> */}
         </div>
       </div>
     </div>
