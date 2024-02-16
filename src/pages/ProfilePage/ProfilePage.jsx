f6c98ad7c9633ecefa6b4e6149af3cd5df8318c8
Venera <veneraomurzakkyzy@gmail.com>
2024-02-16 07:11:22 +0600

    Add profile page header

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -5,8 +5,23 @@ import styles from "./ProfilePage.module.scss";
 
 const ProfilePage = () => {
   return (
-    <div>
+    <div className={styles.profile_container}>
       <SidebarMain />
+
+      <header>
+        <div className={styles.ordertype_wrapper}>
+          <h3 className={styles.orders_title}>Заказы</h3>
+          <button>Профиль</button>
+          <button>График работы</button>
+          <img
+            src={images.bellIcon}
+            alt="The notifications"
+            className={styles.notifications_icon}
+          />
+        </div>
+      </header>
+
+      <section></section>
     </div>
   );
 };
