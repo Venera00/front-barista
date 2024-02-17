d34d0672a9cf16d61c6f7035eb9b941425008260
Venera <veneraomurzakkyzy@gmail.com>
2024-02-17 06:37:21 +0600

    Add about barista info

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -8,20 +8,33 @@ const ProfilePage = () => {
     <div className={styles.profile_container}>
       <SidebarMain />
 
-      <header className={styles.header}>
-        <h3 className={styles.header_title}>Профиль</h3>
-        <div className={styles.btn}>
-          <button className={styles.btn_profile}>Профиль</button>
-          <button className={styles.btn_schedule}>График работы</button>
-        </div>
-        <img
-          src={images.bellIcon}
-          alt="The notifications"
-          className={styles.notifications_icon}
-        />
-      </header>
+      <div>
+        <header className={styles.header}>
+          <h3 className={styles.header_title}>Профиль</h3>
+          <div className={styles.btn}>
+            <button className={styles.btn_profile}>Профиль</button>
+            <button className={styles.btn_schedule}>График работы</button>
+          </div>
+          <img
+            src={images.bellIcon}
+            alt="The notifications"
+            className={styles.notifications_icon}
+          />
+        </header>
 
-      <section className={styles.about}></section>
+        <section className={styles.about}>
+          <div className={styles.about_wrapper}>
+            <h3>Личные данные</h3>
+            <div>
+              <label htmlFor="">Имя</label>
+              <div>Алихандро</div>
+              <div>555 555 555</div>
+              <div>Фамилия</div>
+              <div>14.03.1998</div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
