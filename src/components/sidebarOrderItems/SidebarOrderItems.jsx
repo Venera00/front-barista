4b1a5b748bcc554e7ddc8ff28bbbb9702bd5fcf7
Venera <veneraomurzakkyzy@gmail.com>
2024-02-19 00:50:35 +0600

    T4-95 finish sidebar for displaying order

--- src/components/sidebarOrderItems/SidebarOrderItems.jsx
+++ src/components/sidebarOrderItems/SidebarOrderItems.jsx
@@ -3,6 +3,23 @@ import images from "../../assets/images";
 import styles from "./SidebarOrderItems.module.scss";
 
 const SidebarOrderItems = ({ order }) => {
+  const [openSideBar, setOpenSideBar] = useState(true);
+  const [count, setCount] = useState(1);
+
+  const toggleSideBar = () => {
+    setOpenSideBar(!openSideBar);
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
   // const dishData = [
   //   {
   //     order_title: "Латте",
@@ -12,29 +29,54 @@ const SidebarOrderItems = ({ order }) => {
   // ];
 
   return (
-    <div className={styles.sidebar_container}>
-      <h3>Заказ на вынос</h3>
-      <img src={images.closeIconWhite} alt="Close" />
-      <div>
-        <div className={styles.orderdetails_wrapper}>
-          <img src={images.coffee} alt="The order item" />
-          {/* <div>
-            <h3>{order.order_title}</h3>
-            {dishData.order_details.map((detail, index) => (
-              <p key={index}>{detail}</p>
-            ))}
-          </div> */}
-
+    <>
+      {openSideBar && (
+        <div className={styles.sidebar}>
+          <h3 className={styles.sidebar__title}>Заказ на вынос</h3>
+          <img
+            src={images.closeIconWhite}
+            alt="Close"
+            className={styles.sidebar__close}
+            onClick={toggleSideBar}
+          />
           <div>
-            {/* <p>{order.price}</p> */}
-            <div>
-              <button>+</button>
-              <button>-</button>
+            <div className={styles.orderdetails_wrapper}>
+              <img src={images.coffee} alt="The order item" />
+              {order && (
+                <>
+                  <div>
+                    <h3>{order.order_title}</h3>
+                    {order.order_details.map((detail, index) => (
+                      <p key={index}>{detail}</p>
+                    ))}
+                  </div>
+
+                  <div>
+                    <p>{order.price}</p>
+                  </div>
+                </>
+              )}
+              <div className={styles.sidebar_btns}>
+                <button onClick={increment} className={styles.sidebar_btn}>
+                  +
+                </button>
+                <p>{count}</p>
+                <button onClick={decrement} className={styles.sidebar_btn}>
+                  -
+                </button>
+              </div>
             </div>
           </div>
+
+          <button className={styles.orderadd_btn}>Добавить</button>
+
+          <div className={styles.sidebar__footer}>
+            <p className={styles.footer__title}>Итого</p>
+            <button className={styles.footer__btn}>Принять</button>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
