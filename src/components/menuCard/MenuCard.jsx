6ebc32eb7bcc948021c100d3de356fb844a3e0b6
Venera <veneraomurzakkyzy@gmail.com>
2024-02-23 00:14:02 +0600

    Add counter to menu card

--- src/components/menuCard/MenuCard.jsx
+++ src/components/menuCard/MenuCard.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import images from "../../assets/images";
 import styles from "./MenuCard.module.scss";
 
 const MenuCard = ({ menuItem }) => {
+  const [showBtns, setShowBtns] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+
   const handleBtnClick = () => {
-    console.log("Btn clicked");
+    setShowBtns(true);
   };
+
+  const handleAddItem = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleRemoveItem = () => {
+    // setShowBtns(true);
+    setQuantity(quantity - 1);
+  };
+
   return (
     <div className={styles.menucard}>
       <img
@@ -22,6 +35,18 @@ const MenuCard = ({ menuItem }) => {
       <button onClick={handleBtnClick} className={styles.menucard__btn}>
         +
       </button>
+
+      {showBtns && (
+        <div className={styles.counter}>
+          <button onClick={handleRemoveItem} className={styles.counter__remove}>
+            -
+          </button>
+          <p>{quantity}</p>
+          <button onClick={handleAddItem} className={styles.counter__add}>
+            +
+          </button>
+        </div>
+      )}
     </div>
   );
 };
